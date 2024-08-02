dfda9bdf12f82f95742222987ea5bce7db10a288
Tomas Hrstka <tomas.hrstka@livesport.eu>
2024-08-02 10:33:11 +0200

    add hono and biome

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+const port = 3000;
+
+export default {
+	port: port,
+	fetch: app.fetch,
+};
+
+console.log(`Server running at http://localhost:${port}`);
